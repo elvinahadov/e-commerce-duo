866aa548585ee7791e1b54c05412643984406d1b
Elvin Ahadov <elvineah@code.edu.az>
2024-09-06 17:35:40 +0400

    update header

--- components/layout/header/mainHeader/index.tsx
+++ components/layout/header/mainHeader/index.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
 import Chevron from "@/assets/images/icons/chevron.svg";
 import Search from "@/assets/images/icons/searchIcon.svg";
 import SearchBlack from "@/assets/images/icons/searchIconBlack.svg";
@@ -7,61 +7,69 @@ import Cart from "@/assets/images/icons/cartIcon.svg";
 import Person from "@/assets/images/icons/personIcon.svg";
 import Hamburger from "@/assets/images/icons/hamburgerIcon.svg";
 
+
 const MainHeader = () => {
   return (
     <section className="bg-white">
-      <div className="max-w-[1240px] m-auto flex items-center my-6 gap-10">
-        <div className="hidden">
+      <div className="max-w-[1240px] mx-auto py-6 flex items-center justify-between px-4 gap-10">
+        <div className="flex gap-3 items-center">
           <Image
             src={Hamburger}
-            alt=""
+            width={24}
+            height={24}
+            alt="menu"
+            className="visible lg:invisible"
           />
+          <h1 className="text-[32px] font-bold">SHOP.CO</h1>
         </div>
-        <div>
-          <p className="text-black text-[32px] font-bold">SHOP.CO</p>
-        </div>
-        <div>
-          <ul className="text-[16px] font-[400] flex gap-6">
-            <li className="flex">
-              Shop <Image src={Chevron} alt="" />
+
+        <div className="hidden lg:block">
+          <ul className="flex flex-row gap-4 items-center">
+            <li className="flex gap-1">
+              Shop
+              <Image src={Chevron} width={20} height={20} alt="arrowIcon" />
             </li>
             <li>On Sale</li>
-            <li>New Arriwals</li>
+            <li>New Arrivals</li>
             <li>Brands</li>
           </ul>
         </div>
-        <div className="relative">
+
+        <div className="hidden md:flex mx-10 bg-[#F0F0F0] py-3 px-4 rounded-[62px] flex-grow flex gap-3">
+          <button>
+            <Image
+              src={Search}
+              width={24}
+              height={24}
+              alt="graySearch"
+              className="my-auto"
+            />
+          </button>
           <input
             type="text"
-            className="py-3 px-4 pl-10 bg-[#F0F0F0] rounded-[62px] w-[530px]"
             placeholder="Search for products..."
-          />
-          <Image
-            src={Search}
-            alt="Search Icon"
-            className="absolute left-3 top-3"
+            className="placeholder:text-[16px] font-normal text-[#00000066] bg-[#F0F0F0] outline-none w-full"
           />
         </div>
-        <div className="flex gap-[14px]">
-          <div className="hidden">
+
+        <div className="flex gap-[14px] shrink-0">
+          <button>
             <Image
-            src={SearchBlack}
-            alt=""
-            
+              src={SearchBlack}
+              width={24}
+              height={24}
+              alt="blackSearch"
+              className="visible md:invisible"
             />
-          </div>
-          <div>
-            <Image
-            src={Cart}
-            alt=""
-            />
-          </div>
-          <div>
-            <Image
-            src={Person}
-            alt=""
-            />
-          </div>
+          </button>
+
+          <button>
+            <Image src={Cart} width={24} height={24} alt="bucket" />
+          </button>
+
+          <button>
+            <Image src={Person} width={24} height={24} alt="profile" />
+          </button>
         </div>
       </div>
     </section>
--- components/layout/header/subHeader/index.tsx
+++ components/layout/header/subHeader/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
+import CloseIcon from "@/assets/images/icons/closeIcon.svg"
 
 const SubHeader = () => {
   return (
     <section className='bg-black'>
-      <div className='max-w-[1240px]  m-auto'>
-        <p className='text-white text-[14px] font-[400]  text-center py-[10px]'>Sign up and get 20% off to your first order. 
-          <a href="#" className='text-decoration-line: underline font-[500] text-[16px]'> Sign Up Now.</a>
-        </p>
+      <div className='max-w-[1240px]  m-auto sm:max-w-[390px]'>
+      <div className='px-4 text-center flex items-center justify-between text-[12px] text-white py-[9px] xl:text-[14px]'>
+      <div></div>
+       
+       <h1 className='font-normal'>Sign up and get 20% off to your first order. <a href="#" className='underline'>Sign Up Now</a></h1>
+      
+
+       <button className='invisible md:visible '>
+        <Image src={CloseIcon} width={20} height={20} alt=''/>
+       </button>
+       </div>
       </div>
     </section>
   )
